6744fd9bd7360eddfaf5945bb2fce7d5541d5e83
Ayush Dash <53513005+Ayushdash123@users.noreply.github.com>
2019-08-02 19:44:18 +0530

    Create question2.cpp

---
+++ question2.cpp
@@ -0,0 +1,23 @@
+#include <iostream.h>
+using namespace std;
+
+int main()
+{
+	int first= 1, second = 1;
+	int i = 1, sum = 0;
+	while(i < 4000000)
+	{
+		first = second;
+		second = i;
+		i = first + second;
+		
+		if(i % 2 == 0)
+			sum += i;
+		
+	}
+	
+	cout<<"sum is: "<<sum;
+	
+	
+return 0;	
+}
